2a91cb23dae88a386c2ecfd846424079db08376c
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2022-11-02 20:09:16 +0530

    Update __version__.py

--- seedr/__version__.py
+++ seedr/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "seedr"
 __description__ = "Unofficial API wrapper for seedr.cc ."
 __url__ = "https://github.com/AnjanaMadu/SeedrAPI"
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 __author__ = "Anjana Madu"
 __author_email__ = "hello@anjanamadu.net"
 __license__ = "GPL 3.0"
